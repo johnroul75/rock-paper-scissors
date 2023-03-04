defffd7551fcdf356425a8000d1b8c487a326f59
johnroul75 <118015873+johnroul75@users.noreply.github.com>
2023-03-04 15:33:17 -0600

    Add function that displays rock, paper, or scissors in the console

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Create a function called getComputerChoice that will return one of three options: rock, paper, or scissors.
+
+function getComputerChoice(compChoice){ 
+   let num =  Math.floor(Math.random() * 3) + 1;
+   if(num === 1){
+    num = 'Rock'
+   } else if(num === 2){
+    num = 'Paper'
+   } else {
+    num = 'Scissors'
+   }
+console.log(num);
+return num;
+
+
+}
+
+getComputerChoice();
+
 
