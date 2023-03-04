49fa437c82ed4e954e53a098560862cf7aa01894
johnroul75 <118015873+johnroul75@users.noreply.github.com>
2023-03-04 16:19:52 -0600

    Add function that plays a round of rock paper scissors.
    This function uses conditional statements checking if your selection
    beats the computers selection. If you win, the console log displays a
    winning message. Same goes for losing and if you draw.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Create a function called getComputerChoice that will return one of three options: rock, paper, or scissors.
+// 1. Create a function called getComputerChoice that will randomly return one of three options: rock, paper, or scissors.
 
 function getComputerChoice(compChoice){ 
    let num =  Math.floor(Math.random() * 3) + 1;
+
    if(num === 1){
-    num = 'Rock'
+    num = 'rock'
    } else if(num === 2){
-    num = 'Paper'
+    num = 'paper'
    } else {
-    num = 'Scissors'
+    num = 'scissors'
    }
-console.log(num);
 return num;
 
 
 }
-
 getComputerChoice();
 
 
+function playRound(playerSelection, computerSelection){
+    const win = 'You win!';
+    const lose = 'You lose!';
+    const tie = 'You tie! Try again!';
+    if((playerSelection === 'rock' && computerSelection === 'scissors') 
+    || (playerSelection === 'paper' && computerSelection === 'rock') 
+    || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
+        return win;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')
+            ||(playerSelection === 'paper' && computerSelection === 'scissors')
+            ||(playerSelection === 'scissors' && computerSelection === 'rock')){
+                return lose;
+    } else {
+        return tie
+    }
+
+}
+
+const playerSelection = prompt('Pick: Rock, Paper or Scissors').toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
