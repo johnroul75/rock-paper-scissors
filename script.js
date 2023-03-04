5a43a43b9761aecb274dfdf3a112b58a03624ba2
johnroul75 <118015873+johnroul75@users.noreply.github.com>
2023-03-04 17:24:59 -0600

    Change placement of const variables
    Changed the placement of the const variables to inside the playRound
    function instead of outside of it.

--- script.js
+++ script.js
@@ -16,25 +16,42 @@ return num;
 }
 getComputerChoice();
 
+// 2. Create a function that plays a round of rock paper scissors and console log the results of the round.
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerChoice, computerChoice){
+const playerSelection = prompt('Pick: Rock, Paper or Scissors').toLowerCase();
+const computerSelection = getComputerChoice();
     const win = 'You win!';
     const lose = 'You lose!';
     const tie = 'You tie! Try again!';
     if((playerSelection === 'rock' && computerSelection === 'scissors') 
     || (playerSelection === 'paper' && computerSelection === 'rock') 
     || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
+        console.log(win);
         return win;
     } else if ((playerSelection === 'rock' && computerSelection === 'paper')
             ||(playerSelection === 'paper' && computerSelection === 'scissors')
             ||(playerSelection === 'scissors' && computerSelection === 'rock')){
+                console.log(lose)
                 return lose;
     } else {
-        return tie
+        console.log(tie);
+        return tie;
     }
-
 }
 
-const playerSelection = prompt('Pick: Rock, Paper or Scissors').toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+playRound();
+
+
+        
+// 3. Write a new function which calls the playRound function inside of this one to playa 5 round game that keeps score and reports a winner and loser at the end.
+
+function game(){
+    play
+}
+ 
+// Random pseuocode:
+// create function called game
+// create a variable inside the function called 'tries'
+// 
