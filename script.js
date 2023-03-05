95a07cd359b45a74d40e5222fe66f8c674e9a147
johnroul75 <118015873+johnroul75@users.noreply.github.com>
2023-03-05 10:57:45 -0600

    Complete last function, add new variables, tweaks
    Completed last function, game is now complete.

--- script.js
+++ script.js
@@ -19,39 +19,53 @@ getComputerChoice();
 // 2. Create a function that plays a round of rock paper scissors and console log the results of the round.
 
 function playRound(playerChoice, computerChoice){
-const playerSelection = prompt('Pick: Rock, Paper or Scissors').toLowerCase();
-const computerSelection = getComputerChoice();
+    const playerSelection = prompt('Pick: Rock, Paper or Scissors').toLowerCase();
+    const computerSelection = getComputerChoice();
     const win = 'You win!';
     const lose = 'You lose!';
     const tie = 'You tie! Try again!';
     if((playerSelection === 'rock' && computerSelection === 'scissors') 
     || (playerSelection === 'paper' && computerSelection === 'rock') 
-    || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
-        console.log(win);
+    || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        ++playerScore;
         return win;
     } else if ((playerSelection === 'rock' && computerSelection === 'paper')
             ||(playerSelection === 'paper' && computerSelection === 'scissors')
             ||(playerSelection === 'scissors' && computerSelection === 'rock')){
-                console.log(lose)
+                ++computerScore;
                 return lose;
     } else {
-        console.log(tie);
+        // console.log(tie);
         return tie;
     }
 }
 
-
-playRound();
-
-
         
 // 3. Write a new function which calls the playRound function inside of this one to playa 5 round game that keeps score and reports a winner and loser at the end.
+   
+    let playerScore = 0;
+    let computerScore = 0;
 
 function game(){
-    play
+    let tries = 0;
+
+    for(let i = 0; i < 5; i++){
+        playRound()
+        tries += 1;
+        console.log(`This is try #${tries}`)
+        console.log('**********')        
+
+    }       
+        console.log(`Player Score is ${playerScore}`);
+        console.log(`Computer Score is ${computerScore}`);
+
+        if(playerScore > computerScore){
+            console.log('Player wins!');
+        } else if(computerScore > playerScore){
+            console.log('Computer wins!');
+        } else {
+            console.log('You both tie!');
+        }
 }
- 
-// Random pseuocode:
-// create function called game
-// create a variable inside the function called 'tries'
-// 
+
+game();
